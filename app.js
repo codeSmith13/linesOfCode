7cc5f8fea9d9006f5cd38d58864321f529c2c9e2
codeSmith13 <peeperz13@gmail.com>
2021-05-10 14:14:03 -0500

    third commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-let friends = ["Chris", "Kvoth", "Bast", "RonnieTaborlin", "Arliden"];
+let friends = ["Chris", "Kvoth", "Bast", "Taborlin", "Arliden"];
 
 for (f = 0; f < friends.length; f++){
     let friend = friends[f]; {
-        for (s = 99; s > 0; s--) {
-            let minusOne = s - 1;
-            if (s > 1) {
-            console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
+        for (numCode = 99; numCode > 0; numCode--) {
+            let minusOne = numCode - 1;
+            if (numCode > 1) {
+            console.log(numCode + " lines of code in the file, " + numCode + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
             } else  {
             console.log("1 line of code in the file, 1 line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file");
             }
@@ -14,5 +14,3 @@ for (f = 0; f < friends.length; f++){
     
 };
 
-var theElement = document.getElementById("friend1");
-friends.friend.call(person1);
