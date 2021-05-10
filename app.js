99e41dda85cc16bca6a2461f31f1eee3ceb17d3e
codeSmith13 <peeperz13@gmail.com>
2021-05-10 14:07:57 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,18 @@
+let friends = ["Chris", "Kvoth", "Bast", "RonnieTaborlin", "Arliden"];
+
+for (f = 0; f < friends.length; f++){
+    let friend = friends[f]; {
+        for (s = 99; s > 0; s--) {
+            let minusOne = s - 1;
+            if (s > 1) {
+            console.log(s + " lines of code in the file, " + s + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
+            } else  {
+            console.log("1 line of code in the file, 1 line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file");
+            }
+        }
+    }
+    
+};
+
+var theElement = document.getElementById("friend1");
+friends.friend.call(person1);
