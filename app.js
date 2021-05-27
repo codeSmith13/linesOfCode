fb21a74a56f3c9b317d0dfaba3d91742e8a596b4
codeSmith13 <peeperz13@gmail.com>
2021-05-27 13:41:46 -0500

    lines of code part 2

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-let friends = ["Chris", "Kvoth", "Bast", "Taborlin", "Arliden"];
-// selects friends and decreases the number of codes numCode 
-for (f = 0; f < friends.length; f++){
-    let friend = friends[f]; {
-        for (numCode = 99; numCode > 0; numCode--) {
-            let minusOne = numCode - 1;
-            if (numCode > 1) {
-            console.log(numCode + " lines of code in the file, " + numCode + " lines of code; " + friend + " strikes one out, clears it all out, " + minusOne + " lines of code in the file");
-            } else  {
-            console.log("1 line of code in the file, 1 line of code; " + friend + " strikes one out, clears it all out, no more lines of code in the file");
-            }
-        }
-    }
-    
-};
+let friends = ["Auri", "Kvoth", "Bast", "Ambrose", "Denna"];
+let sing = document.getElementsByClassName("sing")[0];
+// selects friends and decreases the number of codes numCode
+sing.addEventListener("click", function () {
+  for (i = 0; i < friends.length; i++) {
+    let friend = friends[i];
+    let div = document.createElement("div");
 
+    let h3 = document.createElement("h3");
+    h3.textContent = friends[i];
+    h3.className = "h3";
+    div.appendChild(h3);
+    document.body.appendChild(div);
+
+    for (numCode = 99; numCode > 0; numCode--) {
+      let p = document.createElement("p");
+      p.className = "paragraph";
+      div.appendChild(p);
+      let minusOne = numCode - 1;
+      if (numCode > 2) {
+        p.textContent =
+          +numCode +
+          " lines of code in the file, " +
+          numCode +
+          " lines of code; " +
+          friend +
+          " strikes one out, clears it all out, " +
+          minusOne +
+          " lines of code in the file";
+      } else if (numCode === 2) {
+        p.textContent =
+          +numCode +
+          " lines of code in the file, " +
+          numCode +
+          " lines of code; " +
+          friend +
+          " strikes one out, clears it all out, " +
+          minusOne +
+          " line of code in the file";
+      } else if (numCode === 1) {
+        p.textContent =
+          +numCode +
+          " line of code in the file, " +
+          numCode +
+          " line of code; " +
+          friend +
+          " strikes one out, clears it all out, " +
+          minusOne +
+          " line of code in the file";
+      }
+    }
+  }
+});
